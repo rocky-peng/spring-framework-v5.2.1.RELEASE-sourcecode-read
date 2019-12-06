66869eabc1bb3aa30988c46cf3276ab137b389f2
Rocky <rocky.peng@qq.com>
2019-12-07 02:08:15 +0800

    format module: aspects

--- spring-aspects/src/main/java/org/springframework/context/annotation/aspectj/EnableSpringConfigured.java
+++ spring-aspects/src/main/java/org/springframework/context/annotation/aspectj/EnableSpringConfigured.java
@@ -16,13 +16,13 @@
 
 package org.springframework.context.annotation.aspectj;
 
+import org.springframework.context.annotation.Import;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
-import org.springframework.context.annotation.Import;
 
 /**
  * Signals the current application context to apply dependency injection to
@@ -36,8 +36,8 @@ import org.springframework.context.annotation.Import;
  * {@link org.springframework.context.annotation.EnableLoadTimeWeaving @EnableLoadTimeWeaving}.
  *
  * @author Chris Beams
- * @since 3.1
  * @see org.springframework.context.annotation.EnableLoadTimeWeaving
+ * @since 3.1
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
